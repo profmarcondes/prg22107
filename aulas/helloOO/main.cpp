854a85253a41559eaa16229fa78f3a48b4af92cb
Hugo Marcondes <hugo.marcondes@ifsc.edu.br>
2026-03-10 15:21:25 -0300

    evolução do código na aula de 10/03/26

--- aulas/helloOO/main.cpp
+++ aulas/helloOO/main.cpp
@@ -2,9 +2,45 @@
 
 using namespace std;
 
+namespace ifsc {
+	namespace prg22107 {
+		string msg = "Hello PRG22107!";
+	};
+	string msg = "Hello IFSC!";
+};
+
 class World {
+private:
+	string name;
+
+public:
+	string getName();
+	void setName(string value);
+	void sayHello();
 
 };
+
+string name = "hugo";
+string tmp = "marcondes";
+
+string fullname = name + tmp;
+
+string World::getName() {
+	cout << ::name << " / " << name << endl;
+	return name;
+}
+
+void World::setName(string name) {
+	if(name.size())
+	this->name = name;
+}
+
+void World::sayHello(){
+	cout << "Hello " << this->name << "!" << endl;
+}
+
+World global_obj;
+
 
 int main (int argc, char* argv[]) {
 
@@ -12,9 +48,22 @@ int main (int argc, char* argv[]) {
 
 	printf("Hello World! value = %d\n", value);
 
-	cout << "Hello " << value << " helloOO ! value = ";
-	cout << value;
-	cout << endl;
+	cout << ifsc::prg22107::msg << endl;
+	cout << ifsc::msg << endl;
+	cout << value << std::endl;
+
+	World obj1;
+
+	//obj1.name = "Hugo";
+	obj1.sayHello();
+
+	global_obj.setName("Global World");
+	global_obj.sayHello();
+
+	World * ptr = new World;
+
+	ptr->setName("ptr World");
+	ptr->sayHello();
 
 	return 0;
 }
